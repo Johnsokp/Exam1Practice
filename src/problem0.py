73df839664766515a83199f683ae16a939a0218d
johnsokp <johnsokp@rose-hulman.edu>
2017-12-11 19:36:42 -0500

    all but prob 3

--- src/problem0.py
+++ src/problem0.py
@@ -8,8 +8,8 @@ These problems illustrate concepts that previous problems have not emphasized:
   -- animation (Problem 0c)
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Kynon Johnson.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -146,7 +146,7 @@ def problem0a(n):
            since (2 + 4 + 6) is 12, which is NOT odd.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -163,6 +163,11 @@ def problem0a(n):
     #        Simply try a few examples to convince yourself of this.
     #        ASK FOR HELP if you do not understand this hint.
     # ------------------------------------------------------------------
+
+    if sum_of_digits(n) % 2 == 1:
+        return True
+    else:
+        return False
 
 
 def run_test_problem0b():
@@ -218,7 +223,7 @@ def problem0b(n):
            since there are 46 primes between 2 and 200.
      """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -227,6 +232,15 @@ def problem0b(n):
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     ####################################################################
     # ------------------------------------------------------------------
+
+    x = n-1
+    count = 0
+    num = 2
+    for k in range (x):
+        if is_prime(num) == True:
+            count = count + 1
+        num = num + 1
+    return count
 
 
 def run_test_problem0c():
@@ -294,6 +308,20 @@ def problem0c(circle, n, window):
     #   renders with a half-second pause after rendering.
     ####################################################################
     # ------------------------------------------------------------------
+
+    center = circle.center
+    fill = circle.fill_color
+    radius = circle.radius
+    circle = rg.Circle(center, radius)
+    circle.fill_color = fill
+    circle.attach_to(window)
+
+    for _ in range(n):
+        center.x = center.x + (2 * radius)
+        circle = rg.Circle(center, radius)
+        circle.attach_to(window)
+
+    window.render()
 
 
 # ----------------------------------------------------------------------
--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 PRACTICE Test 1, problem 2.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Kynon Johnson.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -102,7 +102,7 @@ def problem2a(circle, rectangle, window):
       :type window:    rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
     # ------------------------------------------------------------------
@@ -110,6 +110,37 @@ def problem2a(circle, rectangle, window):
     #    DIFFICULTY:      6
     #    TIME ESTIMATE:   10 to 15 minutes.
     # ------------------------------------------------------------------
+
+    # from inputs
+    center = circle.center
+    radius = circle.radius
+    fill = circle.fill_color
+    point1 = rectangle._upper_left_corner
+    point2 = rectangle._lower_right_corner
+    outline = rectangle.outline_color
+
+    # drawing
+    circle = rg.Circle(center, radius)
+    circle.fill_color = fill
+    circle.attach_to(window)
+    rectangle = rg.Rectangle(point1, point2)
+    rectangle.outline_color = outline
+    rectangle.attach_to(window)
+
+    window.render()
+    window.continue_on_mouse_click()
+
+    line =  rg.Line(rectangle._upper_right_corner,rectangle._lower_left_corner)
+    line.arrow = 'last'
+    line.attach_to(window)
+
+    window.render()
+    window.continue_on_mouse_click()
+
+    circle.fill_color = outline
+
+    window.render()
+
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
@@ -181,6 +212,26 @@ def problem2b(rect, n, delta, win):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 to 25 minutes.
     # ------------------------------------------------------------------
+
+    # from inputs
+    point1 = rect._upper_left_corner
+    point2 = rect._lower_right_corner
+    fill = rect.fill_color
+
+    # drawing
+    rectangle = rg.Rectangle(point1, point2)
+    rectangle.fill_color = fill
+    rectangle.attach_to(win)
+
+    for _ in range (n-1):
+        point1.x = point1.x - delta
+        point1.y = point1.y - delta
+        point2.x = point2.x + delta
+        point2.y = point2.y + delta
+        rectangle = rg.Rectangle(point1, point2)
+        rectangle.attach_to(win)
+
+    win.render()
 
 
 # ----------------------------------------------------------------------
